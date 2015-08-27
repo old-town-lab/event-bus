123fb1436d5a25dd2c8ab63341ffe7f3602e442d
Malofeykin Andrey <malofeykin@platformasoft.ru>
2015-08-27 13:27:20 +0300

    Создание модуля. Интеграция с CI

---
+++ src/EventManager/Module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link https://github.com/old-town/event-buss
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\EventBuss\EventManager;
+
+/**
+ * Class EventManager
+ *
+ * @package OldTown\EventBuss\EventManager
+ */
+class EventManager
+{
+
+} 
