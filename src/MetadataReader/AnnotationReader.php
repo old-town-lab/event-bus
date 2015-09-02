a711a5eedbf7de54394bf0c1dd56dd32263e05d5
Malofeykin Andrey <malofeykin@platformasoft.ru>
2015-09-02 08:56:18 +0300

    Разработка абстрактной фабрики для создания менеджера шины/конфигурации/и.т.д

---
+++ src/MetadataReader/AnnotationReader.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link https://github.com/old-town/event-buss
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\EventBuss\MetadataReader;
+
+/**
+ * Class AnnotationReader
+ *
+ * @package OldTown\EventBuss\MetadataReader
+ */
+class  AnnotationReader implements ReaderInterface
+{
+    /**
+     * @var
+     */
+    protected $reader;
+}
---
+++ src/MetadataReader/ReaderInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link https://github.com/old-town/event-buss
+ * @author  Malofeykin Andrey  <and-rey2@yandex.ru>
+ */
+namespace OldTown\EventBuss\MetadataReader;
+
+/**
+ * Interface ReaderInterface
+ *
+ * @package OldTown\EventBuss\MetadataReader
+ */
+interface ReaderInterface
+{
+
+}
